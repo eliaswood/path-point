4bc5c4c83a38648f86b461270353920f03a3f70c
Elias Wood <eliaswood12@gmail.com>
2022-03-05 16:53:14 -0500

    refactored regular expressions to try to be able to handle more cases for non-json

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const fs = require('fs');
 
 const data = fs.readFileSync(`./${process.argv[2]}`, 'utf8');
 const addQuotes = data.replace(/(['"])?([a-zA-Z0-9_=]+)(['"])?:/g, '"$2":');
-const addCommaBracket = addQuotes.replace(/}/g, "},");
-const addCommaEqual = addCommaBracket.replace(/t ==/g, 't ==",');
-const addCommaQuote = addCommaEqual.replace(/===/g, '"===');
-const removeTrailing = addCommaQuote.slice(0, -1);
-const addObjBrackets = "{" + removeTrailing + "}";
+const addCommas = addQuotes.replace(/\n/g, ",\n")
+const addStartQuoteToNonBracket = addCommas.replace(/:\s(?!{|\d|")/g, ': "')
+const addEndQuoteToNonBracket = addStartQuoteToNonBracket.replace(/(?<!}|\d|")(,(?!\s\w))/g, '",');
+const addObjBrackets = "{" + addEndQuoteToNonBracket + "}";
 const parse = JSON.parse(addObjBrackets);
 
 const sortKeys = Object.keys(parse).sort((a, b) => b - a);
